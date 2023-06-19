6c282f37efc0672a2f171b67d65a69fe826cc98b
jlogas <juanjlogas@gmail.com>
2023-06-19 10:49:35 -0500

    handlebars

--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
 import express from 'express';
+import ProductManager from './controladores/productManager.js';
 import rutas from './router/products.routes.js';
 import rutasCarritos from './router/car.routes.js';
+import { engine } from 'express-handlebars';
+import path from "path";
+import __dirname from './utils.js';
 
+const producto = new ProductManager();
 const app = express();
 const puerto = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname + "/views"))
+
+app.use("/", express.static(__dirname + "/public"))
+
+app.use("/", async(req, res)=>{
+    let allProducts = await producto.getProducto()
+    res.render("home",
+    {
+        title:"coder",
+        productos: allProducts
+    })
+})
 
 app.use("/api/productos", rutas)
 app.use("/api/carrito", rutasCarritos)
