500d8555d1c51ab885d9c385b9c3a889accc6de7
jlogas <juanjlogas@gmail.com>
2023-06-19 16:40:01 -0500

    socket

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import rutasCarritos from './router/car.routes.js';
 import { engine } from 'express-handlebars';
 import path from "path";
 import __dirname from './utils.js';
+import { Server} from 'socket.io';
+
 
 const producto = new ProductManager();
 const app = express();
@@ -16,7 +18,6 @@ app.use(express.urlencoded({extended:true}));
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 app.set("views", path.resolve(__dirname + "/views"))
-
 app.use("/", express.static(__dirname + "/public"))
 
 app.use("/", async(req, res)=>{
@@ -31,10 +32,13 @@ app.use("/", async(req, res)=>{
 app.use("/api/productos", rutas)
 app.use("/api/carrito", rutasCarritos)
 
-
-
-
-app.listen(puerto, ()=>{
+const httpServer = app.listen(puerto, ()=>{
     console.log("Trabajando por el puerto 8080"); 
+})
+
+const socketServer = new Server(httpServer)
+
+socketServer.on("connection", socket =>{
+    console.log("conectadoS");
 })
 
