9d9a0d42fa80953de5e4c5eb9a617e4d62891b0e
jlogas <juanjlogas@gmail.com>
2023-07-06 08:40:46 -0500

    hash de password

--- src/router/sessions.router.js
+++ src/router/sessions.router.js
@@ -1,25 +1,44 @@
 import { Router } from "express";
 import userModel from "../dao/models/user.js"
-
+import { createHash } from "../utils.js";
+import { isValidPassword } from "../utils.js";
 
 const routerSessions = Router();
 
 routerSessions.post('/register',async(req,res)=>{
-    const result = await userModel.create(req.body);
+
+    const {first_name,last_name,email,age,password}=req.body
+
+    if(!first_name || !last_name || !email || !age || !password) res.status(400).send({status:"error", error: "faltan datos"});//validacion de datos
+    let user={
+        first_name,
+        last_name,
+        email,
+        age,
+        password: createHash(password)
+    }
+    const result = await userModel.create(user);// usuario con el password "hasheado"
     res.send({status:"success",payload:result});
 })
 
 routerSessions.post('/login',async(req,res)=>{
     const {email, password} = req.body;
-    const user = await userModel.findOne({email,password});
-    if(!user) return res.status(400).send({status:"error",error:"Usuario o contraseña incorrectas"});
+    if(!email|| !password) res.status(400).send({status:"error", error: "faltan datos"});//validacion de datos
+
+    const user = await userModel.findOne({email});
+    if(!user) return res.status(400).send({status:"error",error:"Usuario no existe"});//validacion de usuario
+    if(!isValidPassword(user,password)) return res.status(403).send({status:"error",error: "datos erroneos"});
+
+
+    delete user.password;// borrar contrasena con el login
+    req.session.user=user;
 
     req.session.user = {
         name: `${user.first_name} ${user.last_name}`,
         email:user.email
     }
 
-    res.send({status:"success"})
+    res.send({status:"success", payload:user})
 })
 
 routerSessions.post("/logout",(req,res)=>{
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
 import path from "path"
 import { fileURLToPath } from "url"
+import bcrypt from "bcrypt"
+
+export const createHash = password =>bcrypt.hashSync(password,bcrypt.genSaltSync(10))
+
+export const isValidPassword = (user,password)=> bcrypt.compareSync(password,user.password)
+
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
