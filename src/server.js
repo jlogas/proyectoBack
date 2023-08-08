58b53eff240ce531f72deb61faed2a7860191aa5
jlogas <juanjlogas@gmail.com>
2023-08-08 16:05:29 -0500

    start

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ import initializePassport from './config/passport.config.js';
 import { createTransport } from 'nodemailer';
 import compression from 'express-compression'
  
-import { addLogger } from './utils/logger.js';
+import { addLogger } from './utils/logger.js'; 
 import {logger} from './utils/logger.js'
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUiExpress from "swagger-ui-express";
